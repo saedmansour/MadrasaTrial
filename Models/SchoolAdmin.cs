8b898787ede23f3ca8bd875bc276430ce99c4f08
Saed Mansour <saed.mn@gmail.com>
2012-09-26 16:41:17 +0200

    added files

---
+++ Models/SchoolAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Madrasa.Models
+{
+    public class SchoolAdmin
+    {
+        public int id       { get; set; }   // TODO: isn't id a redundancy to userId?
+        public int userId   { get; set; }
+        public int schoolId { get; set; }
+    }
+
+    public class SchoolAdminDbContext : DbContext
+    {
+        public DbSet<SchoolAdmin> dbSet { get; set; }
+    }
+}
---
+++ Models/Subject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Madrasa.Global;
+
+
+
+namespace Madrasa.Models
+{
+    public class Subject
+    {
+        public int      id                      { get; set; }
+        public string   ancestorIdSplitStr      { get; set; }      
+        public string   title                   { get; set; }
+    }
+
+    public class SubjectDbContext : DbContext
+    {
+        public DbSet<Subject> dbSet { get; set; }
+    }
+
+    public class SubjectView
+    {
+        public int              id              { get; set; }
+        public string           title           { get; set; }
+        public string           childrenId      { get; set; }
+        public List<string>     ancestorIdList  { get; set; }   
+
+
+        //
+        //constructor
+        //
+        public SubjectView() {
+        }
+
+        //
+        //constructor
+        //
+        public SubjectView(Subject subject, string splitStr)
+        {
+            //get Father id from string to List
+            this.ancestorIdList = Helper.SeparateByString(subject.ancestorIdSplitStr, splitStr);
+            this.title          = subject.title;
+            this.id             = subject.id;
+
+            //the next generation ancestorsIds
+            if (subject.ancestorIdSplitStr != null)
+            {
+                this.childrenId = subject.ancestorIdSplitStr + splitStr;
+            }
+            this.childrenId += subject.id;
+        }
+    }
+}
